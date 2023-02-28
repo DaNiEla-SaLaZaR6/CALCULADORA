361d0930addffa20132e44121b92ec3e627a6fa5
Daniela salazar <slazardaniela05@gmail.com>
2023-02-28 13:15:02 -0500

    version1 terminada

---
+++ main.js
@@ -0,0 +1,36 @@
+function mostrar(){
+    document.getElementById('display').innerText=resultado
+}
+function operador(operando){
+    switch(operando){
+        case '+':
+            resultado=parseInt(num1.value)+ parseInt(num2.value)
+            simbolo.innerText='+'
+            console.log(resultado)
+            break
+        case '-':
+            resultado=parseInt(num1.value)- parseInt(num2.value)
+            simbolo.innerText='-'
+            console.log(resultado)
+            break
+
+        case '*':
+            resultado=parseInt(num1.value)* parseInt(num2.value)
+            simbolo.innerText='*'
+            console.log(resultado)
+            break
+
+        case '/':
+            resultado=parseInt(num1.value)/ parseInt(num2.value)
+            simbolo.innerText='/'
+            console.log(resultado)
+            break
+        case 'AC':
+            resultado=''
+            simbolo.innerText=''
+            num1.value=''
+            num2.value=''
+            mostrar()
+
+    }
+}
